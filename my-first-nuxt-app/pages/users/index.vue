73203b5904373b4fbc7612d491d88103ef44154a
中村惇志 <0225cevi@gmail.com>
2021-03-03 22:38:46 +0900

    ‘section_012’

--- my-first-nuxt-app/pages/users/index.vue
+++ my-first-nuxt-app/pages/users/index.vue
@@ -8,7 +8,13 @@
 
 <script>
 export default {
-
+  async mounted(){
+    console.log(
+      JSON.stringify(await
+      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+      )
+    )
+  }
 }
 </script>
 
